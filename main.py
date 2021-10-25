f47a06ca3f18b0402225b359f56750ad1ad11b36
M-jpg-ai <78406326+M-jpg-ai@users.noreply.github.com>
2021-10-25 15:27:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+#import os
+#os.startfile('C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe')
+
+
+#"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+"""
+hkbchajvl
+fnsfksd
+zdvdjs
+"""
+
+number1 = input("Number 1")
+print(number1)
